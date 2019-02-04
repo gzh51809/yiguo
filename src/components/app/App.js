de295cff1bf22295fe176725981629301762d804
Chen123 <870017005@qq.com>
2019-02-04 22:55:37 +0800

    使用Flexible布局实现移动端不同大小屏幕的宽高自适应，完成。

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import './ab.scss';
+
+import Abc from '../text.jsx';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="ok"></div>
+        <Abc />
       </div>
     );
   }
---
+++ src/components/text.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './text.scss'
+class text extends Component {
+  render() {
+    return (
+      <div className="tou">
+        头部
+      </div>
+    );
+  }
+}
+
+export default text;
