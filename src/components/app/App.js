0e1c1e19ca8f8b8b62e88076ceac2cd7924e6934
Chen123 <870017005@qq.com>
2019-02-05 15:28:56 +0800

    sass安装出错

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,15 +4,21 @@ import './ab.scss';
 
 import Abc from '../text.jsx';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div>
         <Abc />
+        <Func />
+      </div>
+        
       </div>
     );
   }
 }
 
+function Func(){
+  return <h1>函数组件</h1>;
+}
 export default App;
---
+++ src/components/app/func.jsx
@@ -0,0 +1,4 @@
+function Func(){
+    return <h1>函数式组件</h1>;
+}
+export default Func;
