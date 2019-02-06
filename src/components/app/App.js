c7dc5b0e58c216621075928ce9d4c66c1a3d53d5
Chen123 <870017005@qq.com>
2019-02-07 00:52:16 +0800

    编程式导航&&首页模块

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,13 +5,21 @@ import './App.css';
 import Abc from '../text.jsx';
 import Navbar from './Navbar.jsx';
 import {SaSwitch} from './router.js';
-
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={data:'main'};
+  }
+  handleClick(porps0,porps1,porps2) {
+    //传入时第一个参数指向 this,第二个参数开始才是
+    //接收时相反
+    console.log(porps0,porps1,porps2);
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App" onClick={this.handleClick.bind(this,1,2)}>
         <Abc />
-        <div className="main">
+        <div className={this.state.data}>
           <SaSwitch></SaSwitch>
         </div>
         <Navbar></Navbar>
